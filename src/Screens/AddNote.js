5f26fdce0508703f70f92525a0c71ad11855e861
fdlnfjrrmdni <fdlnfjrrmdni@gmail.com>
2019-07-02 19:56:13 +0700

    update components

--- src/Screens/AddNote.js
+++ src/Screens/AddNote.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { StyleSheet, TouchableOpacity, StatusBar, ScrollView, FlatList } from 'react-native';
-import { Fab, View, Text, Container, Header, Left, Body, Right, Title, 
-    Icon, Button, Input, Textarea, Thumbnail, Content, Item, Picker, Form } from 'native-base';
+import { Container, Content } from 'native-base';
+
+import Headers from '../Components/Headers';
+import FormNote from '../Components/FormNote';
 
 class AddNote extends Component {
     goBack = () => {
@@ -9,117 +10,23 @@ class AddNote extends Component {
         navigation.goBack();
     }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-        selected2: undefined
-        };
-    }
-    onValueChange2(value: string) {
-        this.setState({
-        selected2: value
-        });
-    }
-
     render() {
         return (
             <Container>
-                <Header style={styles.header} androidStatusBarColor='#fff' iosBarStyle='dark-content'>
-                    <View>
-                        <TouchableOpacity style={{ marginLeft: 15 }} transparent onPress={this.goBack}>
-                            <Icon name='arrow-back' />
-                        </TouchableOpacity>
-                    </View>
-                    <View>
-                        <Title style={styles.title}>ADD NOTE</Title>
-                    </View>
-                    <View>
-                        <TouchableOpacity onPress={this.goBack} style={{ marginRight: 15 }} transparent>
-                            <Icon name='checkmark-circle-outline' style={{ color: '#3DB39E' }} />
-                        </TouchableOpacity>
-                    </View>
-                </Header>
+                <Headers 
+                title='ADD NOTE'
+                iconLeft='arrow-back'
+                iconRight='checkmark-circle-outline'
+                iconRightColor='#3DB39E'
+                leftPress={this.goBack}
+                rightPress={this.goBack}
+                />
                 <Content>
-                    <Form>
-                        <View style={styles.input}>
-                            <Input placeholder='ADD TITLE ...' placeholderTextColor='#c4c4c4' style={styles.inputTitle} />
-                            <Textarea rowSpan={9} placeholder='ADD DESCRIPTION ...' placeholderTextColor='#c4c4c4' style={styles.inputDescription} />
-                        </View>
-                        
-                        <View style={styles.categoryBar}>
-                            <Text style={styles.categoryText}>CATEGORY</Text>
-                            <Item picker style={styles.picker}>
-                                <Picker
-                                    selectedValue={this.state.selected2}
-                                    onValueChange={this.onValueChange2.bind(this)}
-                                >
-                                    <Picker.Item label="ADD CATEGORY" value="0" />
-                                    <Picker.Item label="ATM Card" value="1" />
-                                </Picker>
-                            </Item>
-                        </View>
-                    </Form>
+                    <FormNote />
                 </Content>
             </Container>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    input: {
-        padding: 20, marginTop: 40
-    },
-    inputTitle: {
-        fontSize: 24, 
-        fontWeight: '400', 
-        marginLeft: 5
-    },
-    inputDescription: {
-        fontSize: 24, 
-        fontWeight: '400'
-    },
-    categoryBar: {
-        padding: 30, 
-        width: 250
-    },
-    categoryText: { 
-        fontWeight: '600', 
-        marginBottom: 10 
-    },
-    picker: {
-        backgroundColor: '#fff',
-        paddingLeft: 15,
-        borderBottomColor: 'transparent',
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-    },
-    header: {
-        backgroundColor: '#fff',
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 5,
-        },
-        shadowOpacity: 0.34,
-        shadowRadius: 6.27,
-
-        elevation: 10,
-        justifyContent: 'space-between',
-        alignItems: 'center'
-    },
-    title: {
-        fontFamily: 'Open Sans',
-        fontSize: 16,
-        fontWeight: '600',
-        color: '#000',
-        alignSelf: 'center'
-    }
-});
 
 export default AddNote;
--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { StyleSheet, TextInput, 
-        TouchableOpacity, StatusBar, 
-        ScrollView, FlatList, 
-        Modal,  } from 'react-native';
-import { Fab, View, 
-        Text, Container, 
-        Header, Title, 
-        Icon, Thumbnail } from 'native-base';
+import { StyleSheet, TouchableOpacity, 
+        StatusBar, ScrollView, 
+        FlatList, Modal } from 'react-native';
+import { View, Text, Container } from 'native-base';
+
+import Fabs from '../Components/Fabs';
+import Search from '../Components/Search';
+import DummyNotes from '../Assets/DummyData/Notes';
+import Cards from '../Components/Cards';
+import Headers from '../Components/Headers';
 
 class Home extends Component {
 
@@ -38,27 +40,14 @@ class Home extends Component {
                 <StatusBar hidden />
 
                 {/* ============== Header ============== */}
-                <Header style={styles.header} 
-                androidStatusBarColor='#fff' 
-                iosBarStyle='dark-content'>
-                    <View>
-                        <TouchableOpacity style={{marginLeft: 10}} 
-                        onPress={this.toggleDrawer}>
-                            <Thumbnail small 
-                            source={{ uri: 'https://cdn.moneysmart.id/wp-content/uploads/2019/03/08124226/Untitled-design-2.jpeg' }} />
-                        </TouchableOpacity>
-                    </View>
-                    <View>
-                        <Title style={styles.title}>NOTE APP</Title>
-                    </View>
-                    <View>
-                        <TouchableOpacity style={{marginRight: 15}} 
-                        transparent 
-                        onPress={() => { this.setModal(true) }}>
-                            <Icon name='funnel' />  
-                        </TouchableOpacity>
-                    </View>
-                </Header>
+                <Headers
+                  thumbnail
+                  leftPress={this.toggleDrawer}
+                  title='NOTE APP'
+                  rightPress={() => {this.setModal(true)}}
+                  iconRight='funnel'
+                  iconRightColor='#000'
+                />
 
                 {/* ============== Sort Dropdown ============== */}
                 <Modal transparent 
@@ -81,96 +70,28 @@ class Home extends Component {
                 </Modal>
 
                 {/* ============== Search ============== */}
-                <View style={styles.searchBar}>
-                  <TextInput style={{marginLeft: 10,marginRight: 25}} 
-                  placeholder="Search..." />
-                </View>
+                <Search />
 
                 {/* ============== Content ============== */}
                 <ScrollView>
                     <View style={styles.content}>
                         <FlatList
-                            data={[{
-                              date: '24 Jun',
-                              title: 'Lifecycle',
-                              category: 'Learn',
-                              note: 'ComponentDidMount, ComponentWillUnmount',
-                              color: '#2FC2DF'
-                            }, {
-                              date: '23 Jun',
-                              title: 'Hooks',
-                              category: 'Learn',
-                              note: 'UseState, UseEffect, UseConteks, UseStyles, UseReducer, UseRef',
-                              color: '#2FC2DF'
-                            }, {
-                              date: '24 Jun',
-                              title: 'Daily Standup',
-                              category: 'Work',
-                              note: 'Yeserday I\'m learn about react native, Today I will start make application',
-                              color: '#C0EB6A'
-                            }, {
-                              date: '24 Jun',
-                              title: 'Macbook',
-                              category: 'Wishlist',
-                              note: 'I have to buy a Macbook this year',
-                              color: '#FAD06C'
-                            }, {
-                              date: '24 Jun',
-                              title: 'Daily Standup',
-                              category: 'Work',
-                              note: 'Yeserday I\'m learn about react native, Today I will start make application',
-                              color: '#C0EB6A'
-                            }, {
-                              date: '24 Jun',
-                              title: 'Today',
-                              category: 'Personal',
-                              note: 'Install react native, Learn about React Native, Make simple app',
-                              color: '#FF92A9'
-                            }, {
-                              date: '28 Jul',
-                              title: 'Today',
-                              category: 'Personal',
-                              note: 'Install react native, Learn about React Native, Make simple app',
-                              color: '#FF92A9'
-                            }]}
-
-                            renderItem={({item}) =>
-                                <View style={{marginRight: 30}}>
-                                    <TouchableOpacity style={{
-                                        width: 138,
-                                        height: 138,
-                                        borderRadius: 7,
-                                        backgroundColor: item.color,
-                                        shadowColor: "#000",
-                                        shadowOffset: {
-                                          width: 0,
-                                          height: 2,
-                                        },
-                                        shadowOpacity: 0.25,
-                                        shadowRadius: 3.84,
-                                        elevation: 5,
-                                        padding: 12,
-                                        marginBottom: 27
-                                    }} backgroundColor='#red' onPress={this.toNote}>
-                                        <Text style={styles.cardDate}>{item.date}</Text>
-                                        <Text numberOfLines={1} style={styles.cardTitle}>{item.title}</Text>
-                                        <Text numberOfLines={1} style={styles.cardCategory}>{item.category}</Text>
-                                        <Text numberOfLines={4} style={styles.cardContent}>{item.note}</Text>
-                                    </TouchableOpacity>
-                                </View>
-                            }
-
+                            data={ DummyNotes }
                             numColumns={2}
+                            renderItem={({item}) =>
+                                <Cards 
+                                press={this.toNote} 
+                                date={item.date} 
+                                title={item.title} 
+                                category={item.category} 
+                                content={item.note} />
+                            }
                         />
                     </View>
                 </ScrollView>
 
                 {/* ============== Fab Add Note ============== */}
-                <Fab style={styles.fab}
-                onPress={this.toAddNote}>
-                    <Icon style={styles.fabIcon}
-                    name='add' />
-                </Fab>
+                <Fabs press={this.toAddNote}/>
 
             </Container>
         );
@@ -178,16 +99,8 @@ class Home extends Component {
 }
 
 const styles = StyleSheet.create({
-  // Dropdown Sort using Modal
   modal: {
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
+    elevation: 7,
     backgroundColor: '#fff',
     padding: 15,
   },
@@ -195,87 +108,7 @@ const styles = StyleSheet.create({
     marginLeft: 27,
     justifyContent: 'space-between',
     paddingTop: 120
-  },
-  header: {
-    backgroundColor: '#fff',
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 5,
-    },
-    shadowOpacity: 0.34,
-    shadowRadius: 6.27,
-
-    elevation: 10,
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  title: {
-    fontFamily: 'Open Sans',
-    fontSize: 16,
-    fontWeight: '600',
-    color: '#000',
-    alignSelf: 'center'
-  },
-  searchBar: {
-    zIndex: 1,
-    backgroundColor: '#fff',
-    paddingLeft: 15,
-    borderBottomColor: 'transparent',
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 5,
-    },
-    shadowOpacity: 0.34,
-    shadowRadius: 6.27,
-
-    elevation: 10,
-    marginTop: 85,
-    alignSelf:'center',
-    height: 45,
-    width: 307,
-    position: 'absolute',
-    borderRadius: 20
-            },
-  card: {
-    width: 138,
-    height: 138,
-    borderRadius: 7,
-    backgroundColor: '#2FC2DF',
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-    padding: 12,
-    marginBottom: 30
-  },
-  cardDate: {
-    color: '#fff',
-    textAlign: 'right',
-    fontSize: 12,
-    fontWeight: '800'
-  },
-  cardCategory: {
-    color: '#fff',
-    fontSize: 13
-  },  
-  cardTitle: {
-    color: '#fff',
-    fontWeight: '900',
-    fontSize: 18
-  },
-  cardContent: {
-    color: '#fff',
-    fontSize: 13,
-    fontWeight: '500'
-  },
-  fab: { backgroundColor: '#fff', marginBottom: 20 },
-  fabIcon: { color: '#000' }
+  }
 });
 
 export default Home;
